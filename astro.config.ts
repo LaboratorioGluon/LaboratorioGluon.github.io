30fa840473cdcf0822dd81cc166a2bd4dfe944bb
Laboratorio Gluon <laboratoriogluon@gmail.com>
2025-04-01 14:28:35 +0200

    Updated config for githubpages

--- astro.config.ts
+++ astro.config.ts
@@ -10,7 +10,7 @@ import { spectreDark } from './src/ec-theme';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://spectre.louisescher.dev',
+  site: 'https://laboratoriogluon.github.io',
   output: 'static',
   integrations: [
     expressiveCode({
